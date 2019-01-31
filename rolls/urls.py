d64e5c5f6192aa813c88fe77c4131503a8ca7e78
Dan Dietz <danddietz@gmail.com>
2019-01-31 14:29:25 -0400

    Django user auth demo

---
+++ rolls/urls.py
@@ -0,0 +1,17 @@
+"""
+Rolls app urls
+"""
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
+from . import views
+
+app_name = 'rolls'
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='index'),
+    path('create/', views.CreateView.as_view(), name='create'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+]
---
+++ rolls/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model, login, authenticate
+
+from .models import Roll
+
+
+User = get_user_model()
+
+
+class HomeView(TemplateView):
+    template_name = 'rolls/list.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            ctx['rolls'] = Roll.objects.filter(user=self.request.user)
+
+        return ctx
+
+
+# Create your views here.
+class CreateView(TemplateView):
+    template_name = 'rolls/create.html'
+
+    def post(self, request, *args, **kwargs):
+        title = self.request.POST.get('title')
+        year_made = self.request.POST.get('year_made')
+
+        roll = Roll(title=title, year_made=year_made)
+        roll.user = self.request.user
+        roll.save()
+
+        return HttpResponseRedirect(reverse('rolls:index'))
+
+
+# Create your views here.
+class SignupView(TemplateView):
+    template_name = 'registration/signup.html'
+
+    def post(self, request, *args, **kwargs):
+        # sign up user
+        username = self.request.POST.get('username')
+        password = self.request.POST.get('password')
+        password2 = self.request.POST.get('password2')
+
+        # If the password fields don't match send the user back
+        if password != password2:
+            return HttpResponseRedirect(reverse('rolls:signup'))
+
+        # If the username already exists, send the user back
+        user = User.objects.filter(username=username)
+        if user.count() > 0:
+            return HttpResponseRedirect(reverse('rolls:signup'))
+
+        # save user database record using fancy hashing on password
+        User.objects.create_user(username=username, password=password)
+
+        # Authenticate the user checks provided password against the hash
+        user = authenticate(username=username, password=password)
+
+        # Login the user (does the session table/cookie stuff)
+        login(self.request, user)
+
+        return HttpResponseRedirect(reverse('rolls:index'))
